6d651ed5566ef29d77f2ca402a0a41b79d1a3966
Cory Boyd <me@corytheboyd.com>
2025-12-22 12:00:00 -0700

    Add nix bin shims

--- flake.nix
+++ flake.nix
@@ -13,6 +13,12 @@
             go_1_25
             duckdb
           ];
+          shellHook = ''
+            ln -sf ${nodejs_24}/bin/node ./bin/shim/node
+            ln -sf ${nodePackages.pnpm}/bin/pnpm ./bin/shim/pnpm
+            ln -sf ${go_1_25}/bin/go ./bin/shim/go
+            ln -sf ${duckdb}/bin/duckdb ./bin/shim/duckdb
+          '';
         };
     };
 }
