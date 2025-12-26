29eb24167c84b4c3d23253a03afba0f81e4dc2d6
Cory Boyd <me@corytheboyd.com>
2025-12-26 12:58:18 -0700

    WIP lazy search start MUI

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,37 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import appLogo from "/favicon.svg";
 import PWABadge from "./PWABadge.tsx";
-import "./App.css";
+import Typography from "@mui/material/Typography";
+import AddressSearch from "./lib/AddressSearch.tsx";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="vite-project logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>vite-project</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Typography>CORE Power Outage Tracker</Typography>
+      <AddressSearch />
       <PWABadge />
     </>
   );
 }
-
-export default App;
